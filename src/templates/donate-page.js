c1c3343bb7942a4b50d18068c8ee8007f4e5ab88
vsauceda <vsauceda@codeforamerica.org>
2021-08-06 19:33:42 -0700

    Redesign on donate cards

--- src/templates/donate-page.js
+++ src/templates/donate-page.js
@@ -6,7 +6,7 @@ import Content, { HTMLContent } from "../components/Content";
 import {
   Card,
   CardActions,
-  Button,
+  Fab,
   Grid,
   CardContent,
   Typography,
@@ -18,9 +18,39 @@ const useStyles = makeStyles({
     maxWidth: 345,
     textAlign: "center",
     margin: "auto",
+
+    padding: "1rem",
+    backgroundColor: "#FF4300",
+    color: "white",
+    fontSize: "2rem",
+    height: "100%",
+    "@media (max-width: 675px)": {
+      marginTop: "3rem",
+    },
+  },
+  fab: {
+    color: "black",
+    width: "5rem",
+    height: "5rem",
+    fontSize: "1.2rem",
+    fontWeight: "2px",
+    backgroundColor: "white",
+    marginBottom: "3rem",
+    marginTop: "3rem",
+    "@media (max-width: 900px)": {
+      height: "4rem",
+      width: "4rem",
+      marginBottom: "0rem",
+      marginTop: "2rem",
+    
+    },
   },
   media: {
     height: 140,
+    height: "100%",
+    "@media (max-width: 675px)": {
+      marginTop: "3rem",
+    },
   },
 });
 export const DonatePageTemplate = ({ title, content, contentComponent }) => {
@@ -48,63 +78,142 @@ export const DonatePageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <Card className={classes.root}>
-                <AttachMoneyIcon center style={{ marginTop: "3rem" }} />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Donate to New Freedom Washington
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Thank you for choosing to Donate to New Freedom Washington.
-                    Your contributions are truly appreciated by our people.
-                    Please choose a button below that will fit your needs
-                  </Typography>
-                </CardContent>
+              <Typography
+                gutterBottom
+                variant="h3"
+                component="h1"
+                style={{ textAlign: "center", marginBottom: "3rem" }}
+              >
+                Donate to New Freedom Washington
+              </Typography>
+              <Grid container>
+                <Grid item md={6} xs={12}>
+                  <Card className={classes.root}>
+                    <AttachMoneyIcon
+                      center
+                      style={{ marginTop: "3rem", fontSize: "3rem" }}
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h4"
+                        component="h2"
+                        style={{ marginBottom: "2rem" }}
+                      >
+                        One time Payment
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="h2"
+                        style={{ color: "white", fontSize: "1.6rem" }}
+                      >
+                        Please click below to donate a one time payment
+                      </Typography>
+                    </CardContent>
 
-                <CardActions>
-                  <Grid container>
-                    <Grid item md={12} xs={12}>
-                      <Typography>One time Payment</Typography>
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <a href="https://buy.stripe.com/6oE7vf0Yc3pH10s003">
-                        <Button size="small" color="primary">
-                          Donate 100
-                        </Button>
-                      </a>
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <a href="https://buy.stripe.com/dR65n722g2lDcJabIK">
-                        <Button size="small" color="primary">
-                          Donate 500
-                        </Button>
-                      </a>
-                    </Grid>
-                    <Grid item md={12} xs={12}>
-                      <Typography>Set up a recurring payment</Typography>
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <a href="https://buy.stripe.com/dR67vfayM2lD10s144">
-                        <Button size="small" color="primary">
-                          Donate 100
-                        </Button>
-                      </a>
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <a href="https://buy.stripe.com/9AQ2aV22g3pH10s7st">
-                        <Button size="small" color="primary">
-                          Donate 500
-                        </Button>
-                      </a>
-                    </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
+                    <CardActions>
+                      <Grid container>
+                        <Grid item md={12} xs={12}>
+                          <Typography style={{ fontSize: "1.6rem" }}>
+                            Set up a one time Payment
+                          </Typography>
+                        </Grid>
+                        <Grid item md={4} xs={4}>
+                          <a href="https://buy.stripe.com/9AQ2aV22g3pH10s7st">
+                            <Fab
+                             className={classes.fab}
+                            >
+                              25+
+                            </Fab>
+                          </a>
+                        </Grid>
+                        <Grid item md={4} xs={4}>
+                          <a href="https://buy.stripe.com/6oE7vf0Yc3pH10s003">
+                            <Fab
+                          className={classes.fab}
+                            >
+                              100+
+                            </Fab>
+                          </a>
+                        </Grid>
+                        <Grid item md={4} xs={4}>
+                          <a href="https://buy.stripe.com/dR65n722g2lDcJabIK">
+                            <Fab
+                          className={classes.fab}
+                            >
+                              500+
+                            </Fab>
+                          </a>
+                        </Grid>
+                      </Grid>
+                    </CardActions>
+                  </Card>
+                </Grid>
+                <Grid item md={6} xs={12} className={classes.media}>
+                  <Card className={classes.root}>
+                    <AttachMoneyIcon
+                      center
+                      style={{ marginTop: "3rem", fontSize: "3rem" }}
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h4"
+                        component="h2"
+                        style={{ marginBottom: "2rem" }}
+                      >
+                        Recurring Payment
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="h2"
+                        style={{ color: "white", fontSize: "1.6rem" }}
+                      >
+                        Please click below to donate a recurring payments
+                      </Typography>
+                    </CardContent>
 
+                    <CardActions>
+                      <Grid container>
+                        <Grid item md={12} xs={12}>
+                          <Typography style={{ fontSize: "1.6rem" }}>
+                            Set up a recurring payment
+                          </Typography>
+                        </Grid>
+                        <Grid item md={4} xs={4}>
+                          <a href="https://buy.stripe.com/9AQ2aV22g3pH10s7st">
+                            <Fab
+                          className={classes.fab}
+                            >
+                              25+
+                            </Fab>
+                          </a>
+                        </Grid>
+                        <Grid item md={4} xs={4}>
+                          <a href="https://buy.stripe.com/dR67vfayM2lD10s144">
+                            <Fab
+                          className={classes.fab}
+                            >
+                              100+
+                            </Fab>
+                          </a>
+                        </Grid>
+                        <Grid item md={4} xs={4}>
+                          <a href="https://buy.stripe.com/9AQ2aV22g3pH10s7st">
+                            <Fab
+                       className={classes.fab}
+                            >
+                              500+
+                            </Fab>
+                          </a>
+                        </Grid>
+                      </Grid>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              </Grid>
               <PageContent className="content" content={content} />
             </div>
           </div>
