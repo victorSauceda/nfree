ae7dc1ac5e0d82c184cab3a07e762ca6b54ce618
victorSauceda <victorasauceda@gmail.com>
2022-08-08 22:49:44 -0700

    favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,13 +23,13 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href="src/img/favicon-32x32.png"
+          href="../img/favicon-32x32.png"
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href="src/img/favicon-16x16.png"
+          href="../img/favicon-16x16.png"
           sizes="16x16"
         />
 
